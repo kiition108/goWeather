a8ed049e563bc4319b613a6770757484dcf27a59
kiition108 <66868401+kiition108@users.noreply.github.com>
2022-01-19 23:02:16 +0530

    Update index.js
    port change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http=require("http");
 const fs=require("fs");
-var requests=require("requests")
+var requests=require("requests");
+const port=process.env.PORT;
 
 const homefile=fs.readFileSync("home.html","utf-8");
 const replaceVal=(tempVal,orgVal)=>{
@@ -32,4 +33,6 @@ const server=http.createServer((req,res)=>{
             });
     }
 })
-server.listen(3000,"127.0.0.1");
+server.listen(port,()={
+    console.log(`listening to the port no at ${port}`);
+});
